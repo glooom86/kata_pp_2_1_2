78167dd92a1e937d2bf56c72b2fefa5d130916c7
glooom86 <94992697+glooom86@users.noreply.github.com>
2022-02-10 10:43:19 +0500

    Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorld =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(helloWorld.getMessage());
+        HelloWorld helloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(helloWorld == helloWorld2);
+        System.out.println(cat1 == cat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    private String name;
+
+    public Cat(){}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
